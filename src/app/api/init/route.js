60251e398e99947ee014b905cd80cba0e14e1d52
bsheridan25 <bsheridan25@hotmail.com>
2024-11-10 13:36:23 -0500

    Fix file structure and imports

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -1,5 +1,5 @@
 "use client"
-import FullSchedule from './components/FullSchedule';
+import FullSchedule from './components/FullSchedule';  // Note the './' at the start
 
 export default function Home() {
   return (
